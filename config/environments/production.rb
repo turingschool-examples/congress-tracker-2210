31a53e756ff3fd401ac7512894f0633e236ab19c
Meg Stang <mstang15@gmail.com>
2022-09-29 15:33:15 -0600

    Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "congress-tracker-2108_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "congress-tracker-2207_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
