c5e06db7c3452f5fffa4da3337280df8a635f63e
Meg Stang <mstang15@gmail.com>
2022-10-13 09:17:55 -0400

    Add house search for refactor lesson

--- app/controllers/congress_controller.rb
+++ app/controllers/congress_controller.rb
@@ -13,4 +13,19 @@ class CongressController < ApplicationController
       @member = found_members.first
       render "welcome/index"
     end
+
+    def search_state
+      state = params[:state]
+      conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+        faraday.headers["X-API-KEY"] = ENV['govt_api_key']
+      end
+  
+      response = conn.get("/congress/v1/members/house/#{state}/current.json")
+  
+      json = JSON.parse(response.body, symbolize_names: true)
+  
+      @house_members = json[:results]
+      render "welcome/index"
+    end
+    
   end
---
+++ spec/features/search_house_member_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe 'State House Member Search' do
+  describe 'happy path' do
+    it 'allows user to search for house members by state', :vcr do
+        
+      visit root_path
+
+      select "Colorado", :from => "state"
+      click_button 'Search By State'
+
+      expect(page.status_code).to eq 200
+      expect(page).to have_content("7 Results")
+      expect(page).to have_content("Joe Neguse")
+      expect(page).to have_content("Ed Perlmutter")
+    end
+  end
+end
